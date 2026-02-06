04c00d21c2fd79761477fde1e9def7a5bd813e57
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-02-06 21:17:42 +0700

    feat(dto): add request and acc dto

--- internal/dto/auth.dto.go
+++ internal/dto/auth.dto.go
@@ -1,6 +1,10 @@
 package dto
 
-import "github.com/golang-jwt/jwt/v5"
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type JWTClaims struct {
 	UserID int    `json:"id"`
@@ -10,4 +14,10 @@ type JWTClaims struct {
 
 type JWT struct {
 	Token string `json:"token"`
+}
+
+type Account struct {
+	ID          int        `json:"id" example:"1"`
+	Email       string     `json:"email" example:"user@example.com"`
+	LastLoginAt *time.Time `json:"lastlogin_at" example:"2026-02-06T14:30:45+01:00"`
 }
--- internal/dto/request.dto.go
+++ internal/dto/request.dto.go
@@ -5,3 +5,8 @@ type RegisterRequest struct {
 	Password        string `json:"password" binding:"required,min=8" example:"user123@"`
 	ConfirmPassword string `json:"confirm_password" binding:"required,min=8,eqfield=Password" example:"user123@"`
 }
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email" example:"user@example.com"`
+	Password string `json:"password" binding:"required,min=8" example:"user123@"`
+}
