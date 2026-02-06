d063f1a980b98979f3473d082f09b0f0504430ff
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-02-06 17:01:37 +0700

    feat(service): add register function for register feature

--- internal/service/auth.service.go
+++ internal/service/auth.service.go
@@ -1,6 +1,12 @@
 package service
 
 import (
+	"context"
+	"log"
+	"regexp"
+
+	"github.com/NugrahaPancaWibisana/backend-social-media/internal/apperror"
+	"github.com/NugrahaPancaWibisana/backend-social-media/internal/dto"
 	"github.com/NugrahaPancaWibisana/backend-social-media/internal/repository"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
@@ -14,4 +20,37 @@ type AuthService struct {
 
 func NewAuthService(authRepository *repository.AuthRepository, rdb *redis.Client, db *pgxpool.Pool) *AuthService {
 	return &AuthService{authRepository: authRepository, redis: rdb, db: db}
+}
+
+func (as *AuthService) Register(ctx context.Context, req dto.RegisterRequest) error {
+	emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	matched, _ := regexp.MatchString(emailRegex, req.Email)
+	if !matched {
+		return apperror.ErrInvalidEmailFormat
+	}
+
+	tx, err := as.db.Begin(ctx)
+	if err != nil {
+		log.Println("ERROR [service:auth] failed to begin:", err)
+		return err
+	}
+	defer tx.Rollback(ctx)
+
+	id, err := as.authRepository.CreateAccount(ctx, tx, req)
+	if err != nil {
+		log.Println("ERROR [service:auth] failed to create account:", err)
+		return err
+	}
+
+	if err := as.authRepository.CreateUser(ctx, tx, id); err != nil {
+		log.Println("ERROR [service:auth] failed to create user profile:", err)
+		return err
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		log.Println("ERROR [service:auth] failed to commit:", err)
+		return err
+	}
+
+	return nil
 }
