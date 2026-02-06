99f79f7aad199eeb8affdb9c4dbc2ded1a9750a0
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-02-06 18:32:24 +0700

    feat(controller): add register handler

--- internal/controller/auth.controller.go
+++ internal/controller/auth.controller.go
@@ -1,6 +1,17 @@
 package controller
 
-import "github.com/NugrahaPancaWibisana/backend-social-media/internal/service"
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/NugrahaPancaWibisana/backend-social-media/internal/apperror"
+	"github.com/NugrahaPancaWibisana/backend-social-media/internal/dto"
+	"github.com/NugrahaPancaWibisana/backend-social-media/internal/response"
+	"github.com/NugrahaPancaWibisana/backend-social-media/internal/service"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+)
 
 type AuthController struct {
 	authService *service.AuthService
@@ -8,4 +19,76 @@ type AuthController struct {
 
 func NewAuthController(authService *service.AuthService) *AuthController {
 	return &AuthController{authService: authService}
+}
+
+// Register godoc
+//
+//	@Summary		Register new user
+//	@Description	Create a new user account
+//	@Tags			Auth
+//	@Accept			json
+//	@Produce		json
+//	@Param			request	body		dto.RegisterRequest	true	"User registration data"
+//	@Success		201		{object}	dto.ResponseSuccess
+//	@Failure		400		{object}	dto.ResponseError
+//	@Failure		500		{object}	dto.ResponseError
+//	@Router			/auth/register/ [post]
+func (ac *AuthController) Register(ctx *gin.Context) {
+	var req dto.RegisterRequest
+
+	if err := ctx.ShouldBindWith(&req, binding.JSON); err != nil {
+		errStr := err.Error()
+
+		if strings.Contains(errStr, "Email") && strings.Contains(errStr, "required") {
+			response.Error(ctx, http.StatusBadRequest, "Email field cannot be empty")
+			return
+		}
+
+		if strings.Contains(errStr, "Email") && strings.Contains(errStr, "email") {
+			response.Error(ctx, http.StatusBadRequest, "Email must be a valid email address")
+			return
+		}
+
+		if strings.Contains(errStr, "Password") && strings.Contains(errStr, "required") {
+			response.Error(ctx, http.StatusBadRequest, "Password field cannot be empty")
+			return
+		}
+
+		if strings.Contains(errStr, "Password") && strings.Contains(errStr, "min") {
+			response.Error(ctx, http.StatusBadRequest, "Password must be at least 8 characters")
+			return
+		}
+
+		if strings.Contains(errStr, "ConfirmPassword") && strings.Contains(errStr, "required") {
+			response.Error(ctx, http.StatusBadRequest, "Confirm password field cannot be empty")
+			return
+		}
+
+		if strings.Contains(errStr, "ConfirmPassword") && strings.Contains(errStr, "min") {
+			response.Error(ctx, http.StatusBadRequest, "Confirm password must be at least 8 characters")
+			return
+		}
+
+		if strings.Contains(errStr, "ConfirmPassword") && strings.Contains(errStr, "eqfield") {
+			response.Error(ctx, http.StatusBadRequest, "Your password and confirmation password do not match.")
+			return
+		}
+
+		response.Error(ctx, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+
+	err := ac.authService.Register(ctx, req)
+
+	if err != nil {
+		if errors.Is(err, apperror.ErrEmailAlreadyExists) || errors.Is(err, apperror.ErrInvalidEmailFormat) {
+			response.Error(ctx, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		response.Error(ctx, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+
+	response.Success(ctx, http.StatusCreated, "Registration successful", nil)
 }
