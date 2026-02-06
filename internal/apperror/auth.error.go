27d27c18ca0202da69ca084110be6b216a49b7a3
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-02-06 18:30:07 +0700

    feat(apperror): capitalization for expected error message

--- internal/apperror/auth.error.go
+++ internal/apperror/auth.error.go
@@ -3,6 +3,6 @@ package apperror
 import "errors"
 
 var (
-	ErrEmailAlreadyExists = errors.New("email already exists")
-	ErrInvalidEmailFormat = errors.New("email must be a valid email address")
+	ErrEmailAlreadyExists = errors.New("Email already exists")
+	ErrInvalidEmailFormat = errors.New("Email must be a valid email address")
 )
