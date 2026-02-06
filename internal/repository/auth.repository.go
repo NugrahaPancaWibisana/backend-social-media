71edd3951b5db14fdd13f8c6dceabaee10911fdb
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-02-06 16:30:24 +0700

    feat(repository): add create account function for register feature

--- internal/repository/auth.repository.go
+++ internal/repository/auth.repository.go
@@ -1,7 +1,35 @@
 package repository
 
+import (
+	"context"
+	"log"
+	"strings"
+
+	"github.com/NugrahaPancaWibisana/backend-social-media/internal/apperror"
+	"github.com/NugrahaPancaWibisana/backend-social-media/internal/dto"
+)
+
 type AuthRepository struct{}
 
 func NewAuthRepository() *AuthRepository {
 	return &AuthRepository{}
+}
+
+func (ar *AuthRepository) CreateAccount(ctx context.Context, db DBTX, req dto.RegisterRequest) error {
+	sql := `
+		INSERT INTO
+		    accounts (email, password)
+		VALUES
+		    ($1, $2)
+	`
+
+	if _, err := db.Exec(ctx, sql, req.Email, req.Password); err != nil {
+		log.Println(err.Error())
+		if strings.Contains(err.Error(), "duplicate") {
+			return apperror.ErrEmailAlreadyExists
+		}
+		return err
+	}
+
+	return nil
 }
