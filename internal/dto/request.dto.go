289064a7e004828ea56156450972c66c4945ba92
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-02-06 18:31:16 +0700

    feat(dto): add min binding to confirmm password

--- internal/dto/request.dto.go
+++ internal/dto/request.dto.go
@@ -3,5 +3,5 @@ package dto
 type RegisterRequest struct {
 	Email           string `json:"email" binding:"required,email" example:"user@example.com"`
 	Password        string `json:"password" binding:"required,min=8" example:"user123@"`
-	ConfirmPassword string `json:"confirm_password" binding:"required,eqfield=Password" example:"user123@"`
+	ConfirmPassword string `json:"confirm_password" binding:"required,min=8,eqfield=Password" example:"user123@"`
 }
