3681a353ee54835c4c3a3472e07a66fae06171d5
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-02-06 21:16:54 +0700

    feat(apperror): add expected error service for login feature

--- internal/apperror/auth.error.go
+++ internal/apperror/auth.error.go
@@ -6,4 +6,5 @@ var (
 	ErrEmailAlreadyExists = errors.New("Email already exists")
 	ErrInvalidEmailFormat = errors.New("Email must be a valid email address")
 	ErrUserNotFound       = errors.New("User not found")
+	ErrInvalidCredential  = errors.New("Invalid email or password")
 )
