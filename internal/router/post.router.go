6371b5d8bf07217d810491bc7f7e5675247d80ea
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-02-08 04:34:23 +0700

    feat(router): add new route for posts feature

---
+++ internal/router/post.router.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"github.com/NugrahaPancaWibisana/backend-social-media/internal/controller"
+	"github.com/NugrahaPancaWibisana/backend-social-media/internal/middleware"
+	"github.com/NugrahaPancaWibisana/backend-social-media/internal/repository"
+	"github.com/NugrahaPancaWibisana/backend-social-media/internal/service"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
+)
+
+func PostRouter(app *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
+	postRouter := app.Group("/posts")
+	postRouter.Use(middleware.AuthMiddleware())
+
+	userRepository := repository.NewPostRepository()
+	userService := service.NewPostService(userRepository, rdb, db)
+	userController := controller.NewPostController(userService)
+
+	postRouter.POST("", userController.CreatePost)
+}
