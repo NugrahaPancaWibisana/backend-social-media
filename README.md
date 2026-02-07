79367bc4b2ecad0a4f4a603c529cf373a726e71e
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-02-07 09:23:43 +0700

    feat: add seeder, image, and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Backend Social Media
+
+Sebuah aplikasi backend
+
+![System Design](./system_design.svg)
+![ERD Social Media](./erd_social_media.svg)
---
+++ db/seeder/000002_users_table.seeder.sql
@@ -0,0 +1,12 @@
+-- Insert Users
+INSERT INTO users (account_id, name, avatar, bio, deleted_at, created_at, updated_at) VALUES
+(1, 'john_doe', 'https://i.pravatar.cc/150?img=1', 'Coffee lover ☕ | Travel enthusiast 🌍 | Photography', NULL, '2025-01-15 10:00:00', '2026-01-10 15:20:00'),
+(2, 'jane_smith', 'https://i.pravatar.cc/150?img=2', 'Designer | Creative mind | Love art 🎨', NULL, '2025-01-20 11:30:00', '2025-12-05 09:30:00'),
+(3, 'mike_tech', 'https://i.pravatar.cc/150?img=3', 'Software Developer 💻 | Tech geek | Gamer', NULL, '2025-02-01 09:45:00', '2026-01-20 14:10:00'),
+(4, 'sarah_fit', 'https://i.pravatar.cc/150?img=4', 'Fitness coach 💪 | Healthy lifestyle | Yoga instructor', NULL, '2025-02-10 14:20:00', '2025-11-15 16:45:00'),
+(5, 'david_music', 'https://i.pravatar.cc/150?img=5', 'Musician 🎸 | Producer | Music is life', NULL, '2025-02-15 08:10:00', '2026-02-01 10:30:00'),
+(6, 'emma_foodie', 'https://i.pravatar.cc/150?img=6', 'Food blogger 🍕 | Chef | Culinary adventures', NULL, '2025-03-01 10:30:00', '2025-12-20 11:20:00'),
+(7, 'alex_writer', 'https://i.pravatar.cc/150?img=7', 'Writer ✍️ | Storyteller | Book lover', NULL, '2025-03-10 15:45:00', '2026-01-05 08:50:00'),
+(8, 'olivia_photo', 'https://i.pravatar.cc/150?img=8', 'Photographer 📸 | Nature lover | Wanderlust', NULL, '2025-03-20 09:00:00', '2025-12-28 13:40:00'),
+(9, 'dan_deleted', 'https://i.pravatar.cc/150?img=9', 'Deleted account', '2025-12-01 10:00:00', '2025-04-01 11:15:00', '2025-12-01 10:00:00'),
+(10, 'sophia_style', 'https://i.pravatar.cc/150?img=10', 'Fashion blogger 👗 | Style icon | Trendsetter', NULL, '2025-04-15 13:30:00', '2026-01-25 17:15:00');
