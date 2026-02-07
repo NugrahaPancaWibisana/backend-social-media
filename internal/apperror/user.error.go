f8ee0461009705266b2c51c45175704e02b2b561
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-02-08 05:10:21 +0700

    feat(apperror): add new expected error for follow feature

--- internal/apperror/user.error.go
+++ internal/apperror/user.error.go
@@ -3,5 +3,6 @@ package apperror
 import "errors"
 
 var (
-	ErrNoFieldsToUpdate = errors.New("No fields to update")
+	ErrNoFieldsToUpdate     = errors.New("No fields to update")
+	ErrCannotFollowYourself = errors.New("Cannot follow yourself")
 )
